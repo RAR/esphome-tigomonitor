d513efafaa0be614ed95c8594a0da20215b7967b
Anil Ozyalcin <aeozyalcin@gmail.com>
2026-02-16 02:38:21 +0000

    Revert "Update components/tigo_server/tigo_web_server.cpp"
    This reverts commit 8ea3e72e77529b63c610c3b7af7d525dfd884057.

--- components/tigo_server/tigo_web_server.cpp
+++ components/tigo_server/tigo_web_server.cpp
@@ -1811,6 +1811,7 @@ void TigoWebServer::build_yaml_json(PSRAMString& json, const std::set<std::strin
     }
     }
     if (selected_hub_sensors.count("device_count") > 0) {
+      yaml_text.append("  - platform: tigo_monitor\n");
       yaml_text.append("    tigo_monitor_id: tigo_hub\n");
       yaml_text.append("    name: \"Active Device Count\"\n\n");
     }
